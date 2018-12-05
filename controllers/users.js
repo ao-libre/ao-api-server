9848215a209165df0855c1321e95ec5dbc835785
Lucas Recoaro <5874806+RecoX@users.noreply.github.com>
2018-12-06 08:29:13 +1300

    Added Top ten endpoint and some parsing

--- controllers/users.js
+++ controllers/users.js
@@ -6,4 +6,12 @@ app.get("/usersonline", function (req, res) {
     user.getAllUsersConnectedCount(req, res);
 });
 
+app.get("/users", function (req, res) {
+    user.getAllUsers(req, res);
+});
+
+app.get("/userstoptenlevel", function (req, res) {
+    user.getTopTenMaxLeverUsers(req, res);
+});
+
 module.exports = app;
--- models/user.js
+++ models/user.js
@@ -1,13 +1,11 @@
-const db = require('../db.js')
+const db = require('../db.js');
 
-exports.getAll = function(req, res) {
+const publicFieldsFromUsersTable = "`id`, `name`, `level`, `exp`, `elu`, `description` from `users`";
+
+exports.getAllUsers = function(req, res) {
     db.get().query('SELECT * FROM user', function (err, results, fields) {
         if (err) throw err;
-
-        //preguntar por que se ve diferente esto, que un resultado de mysql to json
-        // res.send([{"asd":"ddd"}])
-
-        res.status(200).send(JSON.stringify(results));
+        res.status(200).json(results);
     })
 };
 
@@ -15,17 +13,20 @@ exports.getAllByUser = function(req, res) {
     console.log(req)
     db.get().query('SELECT * FROM user WHERE id = ?', res.userId, function (err, rows) {
         if (err) throw err;
-        res.send(rows);
+        res.status(200).json(results);
     })
 };
 
 exports.getAllUsersConnectedCount = function(req, res) {
     db.get().query('SELECT COUNT(id) as users_connected FROM `user` WHERE is_logged = true;', function (err, results, fields) {
         if (err) throw err;
+        res.status(200).json(results);
+    })
+};
 
-        //preguntar por que se ve diferente esto, que un resultado de mysql to json
-        // res.send([{"asd":"ddd"}])
-
-        res.status(200).send(JSON.stringify(results));
+exports.getTopTenMaxLeverUsers = function (req, res) {
+    db.get().query('SELECT `id`, `name`, `level`, `exp`, `elu`, `description` FROM `user` ORDER BY `level` DESC LIMIT 10;', function (err, results, fields) {
+        if (err) throw err;
+        res.status(200).json(results);
     })
 };
