89701eca7cea9962507d92a06716836f198bb771
Lucas Recoaro <5874806+RecoX@users.noreply.github.com>
2018-12-14 12:54:26 +1300

    Added docs folder

---
+++ docs/api_project.js
@@ -0,0 +1,1 @@
+define({  "name": "Argentum Online Libre",  "version": "0.1.0",  "description": "Public API to obtain data from our server.",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2018-12-13T23:45:45.642Z",    "url": "http://apidocjs.com",    "version": "0.17.7"  }});
