64fe0c3386f7940f22a351b1847eb74bd6e3fc5d
Lucas Recoaro <5874806+RecoX@users.noreply.github.com>
2019-05-06 15:40:30 +1200

    Improved documentation

---
+++ apidoc/accounts.js
@@ -0,0 +1,12 @@
+/**
+ * @api {get} accounts/backupaccountfiles Backup Account Files to database
+ * @apiVersion 0.1.0
+ * @apiName backupaccountfiles
+ * @apiGroup Accounts
+ *
+ */
+
+
+
+
+ 
---
+++ apidoc/charfiles.js
@@ -0,0 +1,95 @@
+/**
+ * @api {get} charfiles/backupcharfiles Backup Charfiles to database
+ * @apiVersion 0.1.0
+ * @apiName backupcharfiles
+ * @apiGroup Charfiles
+ *
+ */
+
+  /**
+ * @api {get} charfiles/file/:name Get charfile by name
+ * @apiVersion 0.1.0
+ * @apiName getCharfile
+ * @apiGroup Charfiles
+ *
+ * @apiParam {String} name Char file unique name.
+ *
+ */
+
+ /**
+ * @api {get} charfiles/user/:name Get char by name
+ * @apiVersion 0.1.0
+ * @apiName getCharByName
+ * @apiGroup Charfiles
+ *
+ * @apiParam {String} name Char unique name.
+ *
+ */
+
+/**
+ * @api {get} charfiles/all Get all chars
+ * @apiVersion 0.1.0
+ * @apiName getAllCharfiles
+ * @apiGroup Charfiles
+ */
+
+/**
+ * @api {get} charfiles/toptenlevel Get top ten level chars
+ * @apiVersion 0.1.0
+ * @apiName getTopTenMaxLeverCharfiles
+ * @apiGroup Charfiles
+ */
+
+/**
+ * @api {get} charfiles/toptengold Get top ten gold chars
+ * @apiVersion 0.1.0
+ * @apiName getTopTenMoreGoldCharfiles
+ * @apiGroup Charfiles
+ */
+
+/**
+ * @api {get} charfiles/toptenmoretimeonline Get top ten more time online chars
+ * @apiVersion 0.1.0
+ * @apiName getTopTenMoreTimeOnline
+ * @apiGroup Charfiles
+ */
+
+/**
+ * @api {get} charfiles/toptenmorehp Get top ten more health points chars
+ * @apiVersion 0.1.0
+ * @apiName getTopTenMoreHp
+ * @apiGroup Charfiles
+ */
+
+/**
+ * @api {get} charfiles/toptenuserkiller Get top ten more Charfiles killed by char
+ * @apiVersion 0.1.0
+ * @apiName getTopTenUserKiller
+ * @apiGroup Charfiles
+ */
+
+/**
+ * @api {get} charfiles/toptenuserkiller Get top ten more npc killed by char
+ * @apiVersion 0.1.0
+ * @apiName getTopTenUserKiller
+ * @apiGroup Charfiles
+ */
+
+/**
+ * @api {get} charfiles/armada Get all chars who belong to Armada
+ * @apiVersion 0.1.0
+ * @apiName getAllArmadaCharfiles
+ * @apiGroup Charfiles
+ */
+
+
+/**
+ * @api {get} charfiles/caos Get all chars who belong to Caos
+ * @apiVersion 0.1.0
+ * @apiName getAllCaosCharfiles
+ * @apiGroup Charfiles
+ */
+
+
+
+
