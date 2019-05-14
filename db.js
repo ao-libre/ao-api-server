0dca4e69c434dd57141005f7d3654a8e9b9d67f0
Recox <5874806+RecoX@users.noreply.github.com>
2019-05-14 18:51:21 +1200

    Added dotenv to hide passwords :)

--- db.js
+++ db.js
@@ -7,11 +7,15 @@ var state = {
 
 exports.connect = function(mode, done) {
     state.pool = mysql.createPool({
-        host: 'db4free.net',
-        user: 'aolibre_database',
-        database: 'aolibre_database',
-        password: 'cocacola',
-        connectTimeout: 30000
+        // host: 'db4free.net',
+        // user: 'aolibre_database',
+        // database: 'aolibre_database',
+        // password: 'cocacola',
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASSWORD,
+        connectTimeout: 120000
     });
 
     state.mode = mode;
