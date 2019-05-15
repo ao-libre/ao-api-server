697d109df28c645e6c1d17b361229929a00f48e4
Recox <5874806+RecoX@users.noreply.github.com>
2019-05-15 22:08:30 +1200

    Edited catch function

--- models/log.js
+++ models/log.js
@@ -58,6 +58,6 @@ exports.backupLogs = async function (req, res) {
         res.status(200).json({ logs: logsInSqlArray });
     } catch (err) {
         res.status(500).send(err)
-        console.error('function backupCharfiles: ' + err)
+        console.error('function backupLogs: ' + err)
     }
 };
