46caefeeffe5318ecb2a339c61c929bfa76f188a
Recox <5874806+RecoX@users.noreply.github.com>
2019-05-16 18:09:47 +1200

    Encapsulated some function

--- utils/server-configuration.js
+++ utils/server-configuration.js
@@ -7,55 +7,59 @@ function getServerIniFile() {
     return ini.decode(fs.readFileSync(serverFilePath, 'utf-8'));
 }
 
+function getParsedGmsFromServerIni() {
+    let serverIni = getServerIniFile();
+    const gameMasters = {
+        admines: [],
+        dioses: [],
+        semidioses: [],
+        consejeros: [],
+        rolemasters: [],
+    };
+    
+    for (i = 1; i <= serverIni.INIT.Admines; i++) {
+        let adminNumber = "Admin" + i;
+        gameMasters.admines.push(serverIni.Admines[adminNumber]);
+    } 
+
+    for (i = 1; i <= serverIni.INIT.Dioses; i++) {
+        let diosNumber = "Dios" + i;
+        gameMasters.dioses.push(serverIni.Dioses[diosNumber]);
+    }
+
+    for (i = 1; i <= serverIni.INIT.SemiDioses; i++) {
+        let semiDiosNumber = "SemiDios" + i;
+        gameMasters.semidioses.push(serverIni.SemiDioses[semiDiosNumber]);
+    } 
+
+    for (i = 1; i <= serverIni.INIT.Consejeros; i++) {
+        let consejeroNumber = "Consejero" + i;
+        gameMasters.consejeros.push(serverIni.Consejeros[consejeroNumber]);
+    } 
+
+    for (i = 1; i <= serverIni.INIT.RolesMasters; i++) {
+        let roleMasterNumber = "RolesMaster" + i;
+        gameMasters.rolemasters.push(serverIni.RolesMasters[roleMasterNumber]);
+    } 
+
+    //Por si las dudas hago esto ya que pueden poner Admin5 y tan solo setear 3 admines y me tira undefined
+    gameMasters.admines = gameMasters.admines.filter(gm => gm !== Boolean);
+    gameMasters.dioses = gameMasters.dioses.filter(gm => gm !== Boolean);
+    gameMasters.semidioses = gameMasters.semidioses.filter(gm => gm !== Boolean);
+    gameMasters.consejeros = gameMasters.consejeros.filter(gm => gm !== Boolean);
+    gameMasters.rolemasters = gameMasters.rolemasters.filter(gm => gm !== Boolean);
+
+    return gameMasters
+}
+
 
 module.exports = {
     getGmsFromServerIni: function() {
-        let serverIni = getServerIniFile();
-        const gameMasters = {
-            admines: [],
-            dioses: [],
-            semidioses: [],
-            consejeros: [],
-            rolemasters: [],
-        };
-        
-        for (i = 1; i <= serverIni.INIT.Admines; i++) {
-            let adminNumber = "Admin" + i;
-            gameMasters.admines.push(serverIni.Admines[adminNumber]);
-        } 
-    
-        for (i = 1; i <= serverIni.INIT.Dioses; i++) {
-            let diosNumber = "Dios" + i;
-            gameMasters.dioses.push(serverIni.Dioses[diosNumber]);
-        }
-    
-        for (i = 1; i <= serverIni.INIT.SemiDioses; i++) {
-            let semiDiosNumber = "SemiDios" + i;
-            gameMasters.semidioses.push(serverIni.SemiDioses[semiDiosNumber]);
-        } 
-    
-        for (i = 1; i <= serverIni.INIT.Consejeros; i++) {
-            let consejeroNumber = "Consejero" + i;
-            gameMasters.consejeros.push(serverIni.Consejeros[consejeroNumber]);
-        } 
-    
-        for (i = 1; i <= serverIni.INIT.RolesMasters; i++) {
-            let roleMasterNumber = "RolesMaster" + i;
-            gameMasters.rolemasters.push(serverIni.RolesMasters[roleMasterNumber]);
-        } 
-    
-        //Por si las dudas hago esto ya que pueden poner Admin5 y tan solo setear 3 admines y me tira undefined
-        gameMasters.admines = gameMasters.admines.filter(gm => gm !== Boolean);
-        gameMasters.dioses = gameMasters.dioses.filter(gm => gm !== Boolean);
-        gameMasters.semidioses = gameMasters.semidioses.filter(gm => gm !== Boolean);
-        gameMasters.consejeros = gameMasters.consejeros.filter(gm => gm !== Boolean);
-        gameMasters.rolemasters = gameMasters.rolemasters.filter(gm => gm !== Boolean);
-    
-        return gameMasters
+        getParsedGmsFromServerIni()
     },
 
     getAllGmsString: function () {
-        const gameMasters = getGmsFromServerIni()
+        const gameMasters = getParsedGmsFromServerIni()
         let gmsNames = '';
     
         //Separo los elementos del array, pero le agrego '
