d8cc5c99c9e65740b11e6af0db42a4d8c1ca8fc0
Recox <5874806+RecoX@users.noreply.github.com>
2019-05-19 04:08:37 +1200

    Deleted unused code

--- controllers/accounts.js
+++ controllers/accounts.js
@@ -6,11 +6,6 @@ app.get("/backupaccountfiles", function (req, res) {
     account.backupAccountFiles(req, res);
 });
 
-app.get("/test/:jojo", function (req, res, jojo) {
-    console.log('123', jojo)
-    res.status(200)
-});
-
 app.get("/resetPassword/:email/:newPassword", function (req, res) {
     //Email es lo mismo cuenta...
     account.resetPassword(req, res, req.params.email, req.params.newPassword);
