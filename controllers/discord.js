4a4b20c8ceff8f166d7f8e1f5c1aac4546085d71
RecoX <lucas.recoaro@gmail.com>
2019-08-30 21:54:07 +1200

    Added send to discord functionality

--- controllers/discord.js
+++ controllers/discord.js
@@ -2,16 +2,22 @@ const express = require('express');
 const app = express();
 const Discord = require('discord.js');
 
-app.post("/sendMessage/", function (req, res) {
+app.post("/sendConnectedMessage/", function (req, res) {
     // We can create embeds using the MessageEmbed constructor
     // Read more about all that you can do with the constructor
     // over at https://discord.js.org/#/docs/main/stable/class/RichEmbed
-    let username = req.body.username
+    let username = req.body.userName
     let desc = req.body.desc
+
+    console.log( req.body.userName,  req.body.desc,  req.body)
+
+    if (desc === "") {
+        desc = "El personaje no tiene descripcion, que alguien entre al juego y le explique como poner algo :)"
+    }
 
     const embed = new Discord.RichEmbed()
         // Set the title of the field
-        .setTitle(`${username} se ha conectado al server Alkon 0.13`)
+        .setTitle(`${username} se ha conectado al server de ROL Alkon 0.13.X`)
         // Set the color of the embed
         .setColor(0x36cd80)
         // Set the main content of the embed
