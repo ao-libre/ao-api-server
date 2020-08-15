a6df647a94f6cedb986a71305d8371e6be72e9df
Lucas Recoaro <lucas.recoaro@gmail.com>
2020-08-15 17:30:43 +1200

    Agregada lista negra de emails, ahora los emails se mandan con una cola y se usan eventos de Nodemailer para enviarlos

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 1337;
 const db = require('./db');
-const emailTransporter = require('./email-transporter');
 const filemanagerMiddleware = require('@opuscapita/filemanager-server').middleware;
 
 app.use(bodyParser.json()); // support json encoded bodies
@@ -28,24 +27,13 @@ app.use('/api/v1/discord', require('./controllers/discord'));
 app.use('/api/v1/admin', require('./controllers/admin'));
 app.use('/api/v1/guilds', require('./controllers/guilds'));
 
-// Create email transporter pool to send emails
-emailTransporter.createTransporter(function(err) {
-    if (err) {
-        console.error('\x1b[31m%s\x1b[0m', 'Unable to connect to Create Email Transporter.')
-        process.exit(1)
-    } else {
-        console.log('\x1b[32m%s\x1b[0m', `Transportador de email general creado con exito para el email: ${process.env.EMAIL}`);
-        console.log('\x1b[38m%s\x1b[0m', `Transportador de email newsletter creado con exito para el email: ${process.env.EMAIL_NEWSLETTER}`);
-    }
-});
-
 // Connect to MySQL on start
 db.connect(function (err) {
     if (err) {
         console.error('\x1b[31m%s\x1b[0m', 'Unable to connect to MySQL.')
         process.exit(1)
     } else {
-        console.log('\x1b[34m%s\x4b[0m', "Conexion a la BD MySQL Exitosa!");
+        console.log('\x1b[34m%s\x4b[0m', "Conexion a la BD MySQL Exitosa.");
     }
 });
 
